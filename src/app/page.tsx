495852bf4f3249d0b059e22e517c74950035c714
[Yuma K] <[3852000151@k12.hi.us]>
2024-09-11 08:54:30 -1000

    VERCEL. WORK.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -130,7 +130,7 @@ export default function HomePage() {
 <div className="mockup-phone border-primary">
   <div className="camera"></div>
   <div className="display">
-    <div className="artboard artboard-demo phone-1">Hi.</div>
+    <div className="artboard artboard-demo phone-1">Hi!</div>
   </div>
 </div>
     </main>
